ffe733e7db1940577206d886562b13497b8f3517
s-rezayi <rezayi.soheil@gmail.com>
2022-12-27 21:38:30 -0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.17",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        // token: "MATIC",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer
+        },
+    },
+}
